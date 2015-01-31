d1968a7d6d5b88d3a3dc7fcf5fab5a9acf17afa9
Kent Peifeng Ke <kepeifeng@gmail.com>
2015-01-31 21:37:11 +0800

    Initial Commit

---
+++ ThingsIWant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThingsIWant
+//
+//  Created by Kent on 15/1/31.
+//  Copyright (c) 2015年 Kent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ThingsIWant/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ThingsIWant
+//
+//  Created by Kent on 15/1/31.
+//  Copyright (c) 2015年 Kent. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
